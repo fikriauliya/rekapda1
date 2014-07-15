3e5a42a8521bf38ddc28018d1323a7ee1d6d0490
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-07-15 17:59:55 +0800

    Set root pat

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'provinces/index'
-
   get 'locations/index'
+  root 'locations#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
